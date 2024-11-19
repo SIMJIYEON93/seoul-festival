a53d8968fa813cd492abd0c58a8f88d80cd8165e
SIMJIYEON93 <930515s@gmail.com>
2024-11-19 17:57:32 +0900

    init

---
+++ src/main/java/com/peoject/seoulfestival/SeoulFestivalApplication.java
@@ -0,0 +1,13 @@
+package com.peoject.seoulfestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulFestivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulFestivalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peoject/seoulfestival/SeoulFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peoject.seoulfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulFestivalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
